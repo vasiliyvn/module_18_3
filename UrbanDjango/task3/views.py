86cb1d2cd4a4666a4c9fd0940da2a6d0253979e9
vasiliyvn <mishadasha1114@gmail.com>
2025-01-20 20:45:22 +0500

    Update views.py

--- UrbanDjango/task3/views.py
+++ UrbanDjango/task3/views.py
@@ -1,5 +1,24 @@
 from django.shortcuts import render
 from django.views.generic import TemplateView
+
+
 # Create your views here.
+
 def platform(request):
-    return render(request,'platform.html')
+    name_1 = 'Главная страница'
+    context = {'name_1': name_1}
+    return render(request, 'platform.html', context)
+
+
+def games(request):
+    name2 = 'Игры'
+    g1 = 'super mario'
+    g2 = 'tank1990'
+    g3 = 'mortal combat'
+    buy = 'купить'
+    context = {'g1': g1, 'g2': g2, 'g3': g3, 'buy': buy, 'Title': name2}
+    return render(request, 'games.html', context)
+
+
+def cart(request):
+    return render(request, 'cart.html')
